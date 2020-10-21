cbb7b501c18bb96b859846fca6d20b426ee2c607
AV-NASH <avinash21213434@gmail.com>
2020-10-21 17:45:52 +0530

    Welcome message is added to master

---
+++ src/main/java/com/cg/employeepayroll/EmployeeDetails.java
@@ -0,0 +1,19 @@
+package com.cg.employeepayroll;
+
+public class EmployeeDetails {
+
+    private String employeeID;
+    private String employeeName;
+    private Long Salary;
+
+    public void addEmployeeDetails(String employeeID, String employeeName, Long Salary) {
+        this.employeeID = employeeID;
+        this.employeeName=employeeName;
+        this.Salary=Salary;
+    }
+
+    @Override
+    public String toString() {
+        return employeeID+" "+employeeName+" "+Salary;
+    }
+}
---
+++ src/main/java/com/cg/employeepayroll/EmployeePayrollService.java
@@ -0,0 +1,14 @@
+package com.cg.employeepayroll;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class EmployeePayrollService {
+
+     public static void main(String[] args) {
+         System.out.println("Welcome to employee payroll service");
+
+    }
+
+}
+
