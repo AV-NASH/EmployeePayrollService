de3cafba308778453159915fc331acb55f6b4dad
AV-NASH <avinash21213434@gmail.com>
2020-10-21 17:52:14 +0530

    Class created to read and write EmployeeDetails to the console

--- src/main/java/com/cg/employeepayroll/EmployeePayrollService.java
+++ src/main/java/com/cg/employeepayroll/EmployeePayrollService.java
@@ -5,10 +5,32 @@ import java.util.Scanner;
 
 public class EmployeePayrollService {
 
-     public static void main(String[] args) {
-         System.out.println("Welcome to employee payroll service");
+    public ArrayList<EmployeeDetails> employeeDetailsArrayList=new ArrayList<EmployeeDetails>();
+
+    public void readEmployeeDetailsFromConsole(){
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("enter the employee id");
+        String empID=scanner.nextLine();
+        System.out.println("enter the employee name");
+        String empName=scanner.nextLine();
+        System.out.println("enter the salary");
+        Long salary=scanner.nextLong();
+        EmployeeDetails employeeDetails=new EmployeeDetails();
+        employeeDetails.addEmployeeDetails(empID,empName,salary);
+        employeeDetailsArrayList.add(employeeDetails);
 
     }
+    public void writeEmployeeDetailsToConsole(){
+        System.out.println("Employee Details are written to the console\n"+employeeDetailsArrayList.toString());
+    }
+     public static void main(String[] args) {
+         System.out.println("Welcome to employee payroll service");
+         EmployeePayrollService employeePayrollService=new EmployeePayrollService();
+         employeePayrollService.readEmployeeDetailsFromConsole();
+         employeePayrollService.writeEmployeeDetailsToConsole();
+
+
+     }
 
 }
 
